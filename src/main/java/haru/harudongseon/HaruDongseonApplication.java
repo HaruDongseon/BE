a6e1a13e2644ac45f0b1807ac7c734732d83bd2d
sh111-coder <ohk9134@naver.com>
2024-02-01 10:43:59 +0900

    feat: init project

---
+++ src/main/java/haru/harudongseon/HaruDongseonApplication.java
@@ -0,0 +1,13 @@
+package haru.harudongseon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaruDongseonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaruDongseonApplication.class, args);
+    }
+
+}
