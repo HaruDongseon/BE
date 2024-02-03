d4587f73413d7d0666a79ce0c0035babfb7dfc17
sh111-coder <ohk9134@naver.com>
2024-02-03 19:54:03 +0900

    feat: 생성일, 수정일 Entity 구현

--- src/main/java/haru/harudongseon/HaruDongseonApplication.java
+++ src/main/java/haru/harudongseon/HaruDongseonApplication.java
@@ -2,8 +2,10 @@ package haru.harudongseon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HaruDongseonApplication {
 
     public static void main(String[] args) {
