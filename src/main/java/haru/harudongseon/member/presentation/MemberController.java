4e645304d8f61c6c5079f36f7167eae4dad4b0df
sh111-coder <ohk9134@naver.com>
2024-02-29 19:23:33 +0900

    feat: 내 정보 수정 API 구현

--- src/main/java/haru/harudongseon/member/presentation/MemberController.java
+++ src/main/java/haru/harudongseon/member/presentation/MemberController.java
@@ -3,12 +3,12 @@ package haru.harudongseon.member.presentation;
 import haru.harudongseon.global.mvc.AuthMemberDto;
 import haru.harudongseon.global.mvc.AuthPrincipal;
 import haru.harudongseon.member.application.MemberService;
+import haru.harudongseon.member.application.dto.MyProfileEditRequest;
 import haru.harudongseon.member.application.dto.MyProfileResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/members")
@@ -21,5 +21,12 @@ public class MemberController {
     public ResponseEntity<MyProfileResponse> getMyProfile(@AuthPrincipal AuthMemberDto authMemberDto) {
         final MyProfileResponse response = memberService.findMyProfile(authMemberDto.memberId());
         return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping("/me")
+    public ResponseEntity<MyProfileResponse> editMyProfile(@AuthPrincipal AuthMemberDto authMemberDto,
+                                                          @Valid @RequestBody MyProfileEditRequest request) {
+        memberService.editMyProfile(authMemberDto.memberId(), request);
+        return ResponseEntity.ok().build();
     }
 }
--- src/test/java/haru/harudongseon/member/presentation/MemberControllerTest.java
+++ src/test/java/haru/harudongseon/member/presentation/MemberControllerTest.java
@@ -1,24 +1,35 @@
 package haru.harudongseon.member.presentation;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+import java.util.stream.Stream;
 
 import haru.harudongseon.common.H2TruncateUtils;
 import haru.harudongseon.common.builder.MemberBuilder;
+import haru.harudongseon.common.fixtures.member.MemberFixtures;
 import haru.harudongseon.global.jwt.JwtService;
+import haru.harudongseon.member.application.dto.MyProfileEditRequest;
 import haru.harudongseon.member.domain.Member;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MemberControllerTest {
@@ -89,11 +100,93 @@ class MemberControllerTest {
         }
     }
 
+    @Nested
+    @DisplayName("내 정보 수정 시")
+    class EditMyProfile {
+
+        @ParameterizedTest
+        @MethodSource("editRequests")
+        @DisplayName("수정에 성공한다.")
+        void success(final MyProfileEditRequest request) {
+            // given
+            final Member savedMember = memberBuilder.defaultMember().build();
+            final Long memberId = savedMember.getId();
+            final String accessToken = jwtService.createAccessToken(memberId);
+
+            // when
+            final ExtractableResponse<Response> response = EDIT_MY_PROFILE_REQUEST(accessToken, request);
+
+            // then
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        }
+
+        private static Stream<Arguments> editRequests() {
+            final String nickname = MemberFixtures.성하_닉네임;
+            final String profileImageUrl = MemberFixtures.성하_프로필_이미지_URL;
+
+            return Stream.of(
+                    Arguments.arguments(new MyProfileEditRequest(nickname, profileImageUrl)),
+                    Arguments.arguments(new MyProfileEditRequest("edited " + nickname, profileImageUrl)),
+                    Arguments.arguments(new MyProfileEditRequest(nickname, "edited " + profileImageUrl)),
+                    Arguments.arguments(new MyProfileEditRequest("edtied " + nickname, "edited " + profileImageUrl))
+            );
+        }
+
+        @Test
+        @DisplayName("존재하지 않는 멤버의 내 정보 수정이면 실패한다.")
+        void fail_edit_not_exist_member_profile() {
+            // given
+            final Long notExistMemberId = -1L;
+            final String accessToken = jwtService.createAccessToken(notExistMemberId);
+            final MyProfileEditRequest request = new MyProfileEditRequest("edited nickname", "edited profileImageUrl");
+
+            // when
+            final ExtractableResponse<Response> response = EDIT_MY_PROFILE_REQUEST(accessToken, request);
+
+            // then
+            assertSoftly(softly -> {
+                softly.assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());
+                softly.assertThat(response.jsonPath().getString("errorMessage")).isEqualTo("해당하는 멤버를 찾을 수 없습니다.");
+            });
+        }
+
+        @ParameterizedTest
+        @CsvSource(value = {":edited profileUrl", "edited nickname:", ":"}, delimiter = ':')
+        @DisplayName("정보가 하나라도 빈 값이면 내 정보 수정이 실패한다.")
+        void fail_edit_profile_request_blank(final String nickname, final String profileImageUrl) {
+            // given
+            final Member savedMember = memberBuilder.defaultMember().build();
+            final Long memberId = savedMember.getId();
+            final String accessToken = jwtService.createAccessToken(memberId);
+            final MyProfileEditRequest request = new MyProfileEditRequest(nickname, profileImageUrl);
+
+            // when
+            final ExtractableResponse<Response> response = EDIT_MY_PROFILE_REQUEST(accessToken, request);
+
+            // then
+            assertSoftly(softly -> {
+                softly.assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+                softly.assertThat(response.jsonPath().getString("errorMessage")).contains("공백일 수 없습니다.");
+            });
+        }
+    }
+
     private static ExtractableResponse<Response> GET_MY_PROFILE_REQUEST(final String accessToken) {
         return RestAssured.given().log().all()
                 .header(HttpHeaders.AUTHORIZATION, JWT_PREFIX + accessToken)
                 .when().log().all()
                 .get("/members/me")
+                .then().log().all()
+                .extract();
+    }
+
+    private static ExtractableResponse<Response> EDIT_MY_PROFILE_REQUEST(final String accessToken, final MyProfileEditRequest request) {
+        return RestAssured.given().log().all()
+                .header(HttpHeaders.AUTHORIZATION, JWT_PREFIX + accessToken)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when().log().all()
+                .patch("/members/me")
                 .then().log().all()
                 .extract();
     }
