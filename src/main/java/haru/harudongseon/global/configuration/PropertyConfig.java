91c445b81434cc32aef5f3a2055e7c7aae34e1f1
sh111-coder <ohk9134@naver.com>
2024-03-18 11:16:43 +0900

    chore: PropertyConfig 패키지 이동

--- src/main/java/haru/harudongseon/global/configuration/PropertyConfig.java
+++ src/main/java/haru/harudongseon/global/configuration/PropertyConfig.java
@@ -1,4 +1,4 @@
-package haru.harudongseon.global.oauth.config;
+package haru.harudongseon.global.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
