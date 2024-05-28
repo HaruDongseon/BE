9f651a776b99cfbe594239b4ea7117985f62feb2
sh111-coder <ohk9134@naver.com>
2024-05-28 21:11:35 +0900

    refactor: LikePlaceStorage에 Member 간접 참조 추가

--- src/main/java/haru/harudongseon/likeplacestorage/domain/LikePlaceStorage.java
+++ src/main/java/haru/harudongseon/likeplacestorage/domain/LikePlaceStorage.java
@@ -19,11 +19,13 @@ public class LikePlaceStorage {
 
     private String name;
 
+    private Long memberId;
+
     @OneToMany
     private List<LikePlace> likePlaces = new ArrayList<>();
 
-    public LikePlaceStorage(final String name, final List<LikePlace> likePlaces) {
+    public LikePlaceStorage(final String name, final Long memberId) {
         this.name = name;
-        this.likePlaces = likePlaces;
+        this.memberId = memberId;
     }
 }
