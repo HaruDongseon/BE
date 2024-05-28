f17b7dd5f16a0368929303d9fe207e4d69015787
sh111-coder <ohk9134@naver.com>
2024-05-28 13:16:19 +0900

    feat: 보관 장소 검색 기능 구현

--- src/test/java/haru/harudongseon/likeplace/domain/LikePlaceRepositoryTest.java
+++ src/test/java/haru/harudongseon/likeplace/domain/LikePlaceRepositoryTest.java
@@ -1,6 +1,7 @@
 package haru.harudongseon.likeplace.domain;
 
 import static haru.harudongseon.common.fixtures.LikePlaceFixtures.기본_보관_장소_엔티티;
+import static haru.harudongseon.common.fixtures.LikePlaceFixtures.기본_보관_장소_엔티티_이름_파라미터;
 import static haru.harudongseon.common.fixtures.MemberFixtures.기본_회원_엔티티;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -54,6 +55,27 @@ class LikePlaceRepositoryTest extends RepositoryTest {
 
         // when
         final List<LikePlace> actual = likePlaceRepository.findAllByMemberId(member.getId());
+
+        // then
+        assertThat(actual).usingRecursiveFieldByFieldElementComparator().isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Member Id와 키워드에 해당하는 보관 장소를 정확도 순(키워드 일치 -> 키워드로 시작 -> 키워드로 끝)으로 조회한다.")
+    void search_by_keyword_and_member_id_with_correct_order() {
+        // given
+        final Member member = memberRepository.save(기본_회원_엔티티());
+        final String keyword = "성수";
+
+        final LikePlace thirdLikePlace = likePlaceRepository.save(기본_보관_장소_엔티티_이름_파라미터(member, "베이커리 " + keyword));
+        final LikePlace secondLikePlace = likePlaceRepository.save(기본_보관_장소_엔티티_이름_파라미터(member, keyword + "지점"));
+        final LikePlace firstLikePlace = likePlaceRepository.save(기본_보관_장소_엔티티_이름_파라미터(member, keyword));
+        final LikePlace fourthLikePlace = likePlaceRepository.save(기본_보관_장소_엔티티_이름_파라미터(member, "스타벅스 " + keyword +"점"));
+
+        final List<LikePlace> expected = List.of(firstLikePlace, secondLikePlace, thirdLikePlace, fourthLikePlace);
+
+        // when
+        final List<LikePlace> actual = likePlaceRepository.searchByKeywordAndMemberId(keyword, member.getId());
 
         // then
         assertThat(actual).usingRecursiveFieldByFieldElementComparator().isEqualTo(expected);
