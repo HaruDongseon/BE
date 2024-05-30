86b5ca4243bdd1c750607f99dd8007a867267c34
sh111-coder <ohk9134@naver.com>
2024-05-30 13:35:09 +0900

    feat: 동선 보관함 도메인 설계 완료

---
+++ src/main/java/haru/harudongseon/routestorage/domain/StoredRoute.java
@@ -0,0 +1,24 @@
+package haru.harudongseon.routestorage.domain;
+
+import haru.harudongseon.route.domain.Route;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class StoredRoute {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private Route route;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private RouteStorage routeStorage;
+}
