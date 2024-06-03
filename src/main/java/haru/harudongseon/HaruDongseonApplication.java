fec217390d044ee0195265158c8e0d142435b0e4
sh111-coder <ohk9134@naver.com>
2024-06-04 00:37:49 +0900

    feat: Retry 로직 추가

--- src/main/java/haru/harudongseon/HaruDongseonApplication.java
+++ src/main/java/haru/harudongseon/HaruDongseonApplication.java
@@ -3,11 +3,13 @@ package haru.harudongseon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableAsync
+@EnableRetry
 public class HaruDongseonApplication {
 
     public static void main(String[] args) {
--- src/main/java/haru/harudongseon/likeplacestorage/application/LikePlaceStorageEventListener.java
+++ src/main/java/haru/harudongseon/likeplacestorage/application/LikePlaceStorageEventListener.java
@@ -7,6 +7,9 @@ import haru.harudongseon.likeplace.application.event.LikePlaceDeleteEvent;
 import haru.harudongseon.likeplacestorage.domain.StoredLikePlace;
 import haru.harudongseon.likeplacestorage.domain.StoredLikePlaceRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,9 +25,19 @@ public class LikePlaceStorageEventListener {
 
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Retryable(
+        maxAttempts = 3,
+        backoff = @Backoff(delay = 1000)
+    )
     public void deleteLikePlace(final LikePlaceDeleteEvent likePlaceDeleteEvent) {
         final Long likePlaceId = likePlaceDeleteEvent.likePlaceId();
         final List<StoredLikePlace> storedLikePlaces = storedLikePlaceRepository.findAllByLikePlaceId(likePlaceId);
         storedLikePlaces.forEach(StoredLikePlace::unstored);
+    }
+
+    @Recover
+    public void recoverDeleteLikePlace(final LikePlaceDeleteEvent likePlaceDeleteEvent) {
+        final Long likePlaceId = likePlaceDeleteEvent.likePlaceId();
+
     }
 }
